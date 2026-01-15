333d0066dacd870746f6e88ad4e587f71833b280
Mattias <mattias.bygge@gmail.com>
2026-01-15 21:35:08 +0100

    branch done, ready to merge

---
+++ server/db.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+
+const DB_PORT = process.env.DB_PORT ?? "27017";
+const DB_NAME = process.env.DB_NAME;
+const MONGODB_URI =
+  process.env.MONGO_URI ?? `mongodb://127.0.0.1:${DB_PORT}/${DB_NAME}`;
+
+if (!DB_NAME) {
+  throw new Error("DB_NAME is not defined in environment variables");
+}
+
+await mongoose.connect(MONGODB_URI);
+console.log("Connected to db:", MONGODB_URI);
+
+export default mongoose;
---
+++ server/router.ts
@@ -0,0 +1,26 @@
+import express from "express";
+
+import { postBook } from "./controllers/books.js";
+import {
+  getUserBooks,
+  updateUserBookStatus,
+  updateUserBookOwned,
+  updateUserBookFavorite,
+  updateUserBookProgress,
+  updateUserBookFormat,
+  updateUserBookShelves,
+} from "./controllers/userBooks.js";
+
+const router = express.Router();
+
+router.post("/books", postBook); // add book to user
+router.get("/books", getUserBooks); // all books
+router.put("/userbooks/:bookId/status", updateUserBookStatus); // update book status
+router.put("/userbooks/:bookId/owned", updateUserBookOwned); // update book owned
+router.put("/userbooks/:bookId/favorite", updateUserBookFavorite); // update book favorite
+router.put("/userbooks/:bookId/progress", updateUserBookProgress); // update book progress
+router.put("/userbooks/:bookId/format", updateUserBookFormat); // update book format
+router.put("/userbooks/:bookId/shelves", updateUserBookShelves); // update book shelves
+// TODO: add all endpoints here
+
+export default router;
