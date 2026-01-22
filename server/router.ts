d80a4cc25f5b75164a103f2a7dabedf3eb883698
Mattias <mattias.bygge@gmail.com>
2026-01-22 10:34:37 +0100

    changed one endpoint in bookservice file

--- server/router.ts
+++ server/router.ts
@@ -14,7 +14,7 @@ import {
 const router = express.Router();
 
 router.post("/books", postBook); // add book to user
-router.get("/userbooks", getUserBooks); // TO DO: Remember to change it on the frontend
+router.get("/userbooks", getUserBooks); // send all userbooks
 router.put("/userbooks/:bookId/status", updateUserBookStatus); // update book status
 router.put("/userbooks/:bookId/owned", updateUserBookOwned); // update book owned
 router.put("/userbooks/:bookId/favorite", updateUserBookFavorite); // update book favorite
